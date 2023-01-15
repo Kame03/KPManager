85a0af612f30510be21cc5277a09a0f4176bbe23
Kame03 <viktor@milivojevic.in.rs>
2023-01-15 18:38:04 +0100

    Replaced ipapi.co with ip-api.com because ipapi.co would always rate-limit us, or cause issues because it's behind cloudflare. Added error message in case player joins from localhost or API returns an error.

--- src/main/java/in/rs/milivojevic/KPManager/JoinWelcomer.java
+++ src/main/java/in/rs/milivojevic/KPManager/JoinWelcomer.java
@@ -16,14 +16,17 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.Objects;
 
+// import static org.bukkit.Bukkit.getLogger;
+
 public final class JoinWelcomer implements Listener {
     private final OkHttpClient client = new OkHttpClient();
     private final String messagePlayer = ChatColor.AQUA + "Welcome " + ChatColor.GREEN + "{playerName}" + ChatColor.AQUA + " to my server! " + ChatColor.GREEN + "Enjoy your stay!";
     private final String messageEveryone = ChatColor.AQUA + "{playerName} joined from " + ChatColor.GREEN + "{country}.";
+    private final String messageError = ChatColor.RED + "Welcome, " + ChatColor.YELLOW + "{playerName}, " + ChatColor.RED + "to the server! It seems you may have joined from localhost or there was an error with the API ip-api.com. Please check your internet connection and firewall settings.";
 
     private String parseCountry(String xml) {
         Document doc = Jsoup.parse(xml, "", org.jsoup.parser.Parser.xmlParser());
-        Elements elements = doc.select("country_name");
+        Elements elements = doc.select("country");
         return elements.text();
     }
 
@@ -35,16 +38,21 @@ public final class JoinWelcomer implements Listener {
         event.setJoinMessage(null);
 
         Request requestIp;
-        requestIp = new Request.Builder().url("https://ipapi.co/" + playerIP + "/xml/").build();
+        requestIp = new Request.Builder().url("http://ip-api.com/xml/" + playerIP).build();
 
         player.sendMessage(messagePlayer.replace("{playerName}", player.getName()));
+
+
 
         try (Response response = client.newCall(requestIp).execute()) {
             assert response.body() != null;
             String body = response.body().string();
             String country = parseCountry(body);
+           // getLogger().info(player.getName() + "Joined from " + country);
             if (!country.isEmpty()) {
                 Bukkit.broadcastMessage(messageEveryone.replace("{playerName}", player.getName()).replace("{country}", country));
+            } else {
+                Bukkit.broadcastMessage(messageError.replace("{playerName}", player.getName()));
             }
         } catch (IOException e) {
             e.printStackTrace();
